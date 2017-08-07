de26867208b5862e94e173e90ad289c5d0d40327
Matthew Weeks <weeksling@gmail.com>
2017-08-07 14:19:25 -0400

    Added odels and admin section for Locations and Products

--- server/api/models.py
+++ server/api/models.py
@@ -2,5 +2,28 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+@python_2_unicode_compatible
+class Product (models.Model) :
+	description = models.CharField(max_length=200)
+	product_id = models.AutoField(primary_key=True)
+
+	def __str__(self) :
+		return self.description
+
+@python_2_unicode_compatible
+class Location (models.Model) :
+	datetime = models.DateTimeField()
+	latitude = models.DecimalField(max_digits=9, decimal_places=7)
+	longitude= models.DecimalField(max_digits=9, decimal_places=7)
+	elevation= models.IntegerField()
+
+	product = models.ForeignKey(Product)
+
+	date_created = models.DateTimeField(auto_now_add=True)
+	last_modified= models.DateTimeField(auto_now = True)
+
+	def __str__(self) :
+		return self.product + " " + self.latitude + " " + self.longitude + " " + self.elevation
+
